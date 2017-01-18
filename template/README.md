1c2cd2bd8722ab834debbee3575cd49c551ccce4
Jensen Bernard <jensenbernard5@gmail.com>
2017-01-18 16:22:42 +0100

    Update template

--- template/README.md
+++ template/README.md
@@ -1,3 +1,9 @@
 # {{ name }}
 
 {{ description }}
+
+```js
+$ yarn install && yarn build && yarn start
+```
+
+The rest is up to you! Happy coding! 🎉
--- template/src/index.js
+++ template/src/index.js
@@ -1,9 +1,17 @@
+#!/usr/bin/env node
+
 // @flow
 
+{{#if lodash}}
+// import _ from 'lodash';
+
+{{/if}}
 import fs from 'fs';
 import path from 'path';
 import chalk from 'chalk';
-import winston from 'winston';{{#if commander}}
+import winston from 'winston';{{#if cheerio}}
+import cheerio from 'cheerio';
+import request from 'request';{{/if}}{{#if commander}}
 import commander from 'commander';{{/if}}
 
 {{#if commander}}
@@ -18,3 +26,16 @@ commander
 {{/if}}
 winston.info(`${chalk.green('Hello world!')}`);{{#if commander}}
 if (commander.sample) { winston.info(`${chalk.blue('Sample: ')} true`); }{{/if}}
+{{#if cheerio}}
+
+request('https://news.ycombinator.com', (error, response, html) => {
+  if (!error && response.statusCode === 200) {
+    const $ = cheerio.load(html);
+    winston.info(chalk.green('Scraper is ready!'));
+    winston.info('I scraped HackerNews as an example:');
+    $('a.storylink').each((i, element) => {
+      winston.info(`${chalk.cyan('Title: ')} ${$(element).text()}`);
+    });
+  }
+});
+{{/if}}
